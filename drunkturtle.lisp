fcdb62426b06b781d494bbec64e607b5ced79b42
Christiaan Janssen <christiaanjanssen@outlook.com>
2020-02-21 18:00:07 +0100

    Remove project root var

--- drunkturtle.lisp
+++ drunkturtle.lisp
@@ -2,7 +2,7 @@
 (in-package #:drunkturtle)
 
 (defvar *acceptor* nil)
-(defvar *project-root* (asdf:system-source-directory :retro-games))
+;(defvar *project-root* (asdf:system-source-directory :retro-games))
 
 
 (defmacro standard-page ((&key title) &body body)
@@ -27,9 +27,3 @@
       (:p "Under construction!")))
 
 
-(defun start-server (port)
-  (start (setf *acceptor*
-	       (make-instance
-		'easy-acceptor :port port
-			       :document-root
-			       (merge-pathnames "static/" *project-root*)))))
